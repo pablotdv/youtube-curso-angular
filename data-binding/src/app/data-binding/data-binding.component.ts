834f40388cf9496510c648447f6c7477cabbf679
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-03-21 20:06:23 -0300

    Curso Angular #13: Reusando Componentes com Input properties

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -13,7 +13,9 @@ export class DataBindingComponent implements OnInit {
   valorAtual: string = '';
   valorSalvo: string = '';
   isMouseOver: boolean = false;
-  
+
+  nomeDoCurso: string = 'Angular';
+
 
   constructor() { }
 
