194a4d0005552661c21ac1bac78573c689f2896d
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-03-24 16:47:55 -0300

    Curso Angular 2 #27: Diretivas: ngFor

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TesteComponent } from './teste/teste.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch
     AppComponent,
     DiretivaNgifComponent,
     TesteComponent,
-    DiretivaNgswitchComponent
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent
   ],
   imports: [
     BrowserModule
