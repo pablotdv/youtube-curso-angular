360c94de5518d3017acf9f8ff8f415cbfdf9166b
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-04-03 16:41:21 -0300

    Curso Angular #42: Injetando um serviço em outro serviço

--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { LogService } from '../shared/log.service';
 
 
 @Injectable()
@@ -8,15 +9,17 @@ export class CursosService {
   static criouNovoCurso = new EventEmitter<string>();
 
   cursos: string[] = ['Angular 2', 'C#', 'Xamarin'];
-  constructor() {
+  constructor(private log: LogService) {
     console.log('CursosService');
   }
 
   getCursos() {
+    this.log.consoleLog('Obtendo lista de cursos!');
     return this.cursos;
   }
 
   addCurso(curso: string) {
+    this.log.consoleLog(`Criando um novo curso ${curso}`);
     this.cursos.push(curso);
     this.emitirCursoCriado.emit(curso);
     CursosService.criouNovoCurso.emit(curso);
