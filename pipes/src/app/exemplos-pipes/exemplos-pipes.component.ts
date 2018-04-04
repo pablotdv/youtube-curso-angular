40045493c4884a2582effab2891895f25141baec
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-04-04 13:29:29 -0300

    Pipes

---
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent implements OnInit {
+
+  livro: any = {
+    titulo: 'Learning JavaScript Data Structures and Algorithms - Second Edition: Hone your skills by learning classic data structures and algorithms in JavaScript',
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co/i6oGruv'
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
