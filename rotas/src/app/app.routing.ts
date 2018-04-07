2d3476bbd6cd3f84f165482d1b286d5fab4c6f92
Pablo Tôndolo de Vargas <pablotdvsm@gmail.com>
2018-04-07 19:11:16 -0300

    Curso Angular #50: Rotas: Configurando rotas simples

---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+const APP_ROUTES: Routes = [
+    { path: 'Cursos', component: CursosComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '', component: HomeComponent }
+];
+
+export const routing: ModuleWithProviders =
+    RouterModule.forRoot(APP_ROUTES);
